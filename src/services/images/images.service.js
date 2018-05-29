9a0212459a4edcb7b87800fe9eb5522b3f0ed6dd
Grzegorz Leoniec <greg@app-interactive.de>
2018-05-29 22:16:03 +0200

    improved image handling by checking if result is an actual image

--- src/services/images/images.service.js
+++ src/services/images/images.service.js
@@ -4,10 +4,13 @@ const fetch = require('node-fetch');
 module.exports = function (app) {
   return app.use('/images', (req, res) => {
     if (!req.query.url) {
-      res.end('no url found.');
+      res.status(404).end('no url found.');
     }
     fetch(req.query.url)
       .then(result => {
+        if (result.headers.get('content-type').indexOf('image') < 0) {
+          res.status(404).end('no url found.');
+        }
         res.writeHead(200, {
           'Content-Type': result.headers.get('content-type')
         });
